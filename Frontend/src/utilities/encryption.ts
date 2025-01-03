cecf841fac4bbead54b1550c85dad0f690b49090
StaphoneWizzoh <staphoneomondi@gmail.com>
2025-01-03 20:40:54 +0300

    Refactor App component to use React Router for navigation and add encryption utility functions for data security

---
+++ Frontend/src/utilities/encryption.ts
@@ -0,0 +1,49 @@
+import CryptoJS from "crypto-js";
+
+import { ENCRYPTION_KEY } from "../assets/env";
+
+// Function to encrypt user data
+export const encryptData = (data: string) => {
+    if (data == "") {
+        return "";
+    }
+    try {
+        const encryptedData = CryptoJS.AES.encrypt(
+            JSON.stringify(data),
+            ENCRYPTION_KEY
+        ).toString();
+        return encryptedData;
+    } catch (error) {
+        console.error("Encryption error:", error);
+        return null;
+    }
+};
+
+// Function to decrypt user data
+export const decryptData = (encryptedData: unknown) => {
+    if (encryptedData) {
+        try {
+            const bytes = CryptoJS.AES.decrypt(
+                encryptedData as string,
+                ENCRYPTION_KEY
+            );
+            const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+
+            // Check if decryptedData is not empty
+            if (decryptedData) {
+                return JSON.parse(decryptedData);
+            } else {
+                console.log("Error decrypting data: Data NULL ");
+                return null;
+            }
+        } catch (error) {
+            console.error("Decryption error:", error);
+            return null;
+        }
+    } else {
+        console.error(
+            `${encryptedData} passed to the decryptData function is null`
+        );
+        return null;
+    }
+};
