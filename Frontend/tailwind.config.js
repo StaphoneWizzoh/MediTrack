0326a25f0ad223bca2749fe5a34c41b73cdb095a
StaphoneWizzoh <staphoneomondi@gmail.com>
2025-01-03 20:40:16 +0300

    Add @types/crypto-js to devDependencies and enhance Tailwind configuration with custom fonts and colors

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                mulish: ["Mulish", "sans-serif"],
+            },
+            colors: {
+                turquoise: "#5acccc",
+                steelBlue: "#335c6e",
+                teal: "#4aa088",
+                yellowDark: "#faad00",
+                orangeRed: "#f76434",
+            },
+            scrollbar: (theme) => ({
+                thin: {
+                    width: "0.375rem",
+                    backgroundColor: "#f1f1f1",
+                    thumbColor: "#888",
+                    thumbHoverColor: "#555",
+                },
+                default: {
+                    width: "0.5rem",
+                    backgroundColor: "#f1f1f1",
+                    thumbColor: "#585858",
+                    thumbHoverColor: "#555",
+                },
+            }),
+        },
+    },
+    variants: {
+        extend: {
+            scrollbar: ["rounded"],
+        },
     },
     plugins: [],
 };
