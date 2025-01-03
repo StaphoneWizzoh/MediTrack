fcb723c53ee304ad210d244d81c35df9314a0c24
StaphoneWizzoh <staphoneomondi@gmail.com>
2025-01-03 23:05:34 +0300

    Add constants and API configuration for authentication and encryption

---
+++ Frontend/src/assets/constants.ts
@@ -0,0 +1,11 @@
+// URL constants
+
+// export const BASE_URL = "http://127.0.0.1:8000/api/";
+export const BASE_URL = "http://localhost:8000/api/";
+export const SOCKET_URL = "";
+
+export const USER_URL = "http://127.0.0.1:8000/dj-rest-auth/user";
+
+// Other constants
+export const allowedImageExtensions = ["jpg", "jpeg", "png"];
+export const allowedAudioExtensions = ["mp3", "wav"];
