a8072f6bc69fcff1579e1f4a527d7ef10dc1b0fe
StaphoneWizzoh <staphoneomondi@gmail.com>
2025-01-04 18:46:28 +0300

    Add typed hooks for dispatch and selector in Redux store

--- Frontend/src/features/store.ts
+++ Frontend/src/features/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 // import { decryptData } from "../utilities/encryption";
 
 import loginReducer from "./auth/loginSlice";
@@ -6,6 +7,9 @@ import signupReducer from "./auth/signupSlice";
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 // Check if encrypted user data exists in local storage
 // const encryptedUserData = localStorage.getItem("User");
