fd1c261149fb599945fb9dfdc90a0f0ec6885f7c
StaphoneWizzoh <staphoneomondi@gmail.com>
2025-01-04 18:46:04 +0300

    Add URL patterns for serving static files and React assets

--- Backend/backend/urls.py
+++ Backend/backend/urls.py
@@ -1,7 +1,26 @@
+import os
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("api.urls"))
+    path('api/', include('api.urls')),
+
+    # Serve static files
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^assets/(?P<path>.*)$', serve,
+            {'document_root': os.path.join(settings.REACT_APP_DIR, 'assets')}),
+
+    # This should be the last URL pattern
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
+
+# Add this only during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
